75b3ef1ce700e55db8ffcffeadd552b9a67db481
Bowie <gabriel_scorpion_fut@hotmail.com>
2023-11-11 06:15:44 -0300

    operadores

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,42 @@
+
+public class Operadores {
+    public static void main(String[] args) {
+        // String nome = "GABRIEL";
+        // int idade = 31;
+        // double peso = 68.5;
+        // char sexo = 'M';
+        // boolean doadorOrgao = false;
+        // Date dataNascimento = new Date ();
+
+        // double soma = 10.5 + 15.6;
+        // int subtracao = 113 - 20;
+        // int multiplicacao = 20 * 6;
+        // int divisao = 10 / 2;
+        // int modulo = 18 % 3;
+        // double resultado = (10 * 7) + (15 / 3);
+
+        // String nomeCompleto = "LINGUAGEM" + "JAVA";
+
+        String concatenacao = "?";
+
+        concatenacao = 1 + 1 + 1 + "1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1 + "1" + 1 + 1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1 + "1" + 1 + "1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1" + 1 + 1 + 1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1" + (1 + 1 + 1);
+
+        System.out.println(concatenacao);
+    }
+}
--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,24 +1,24 @@
 public class TiposVariaveis {
     public static void main(String[] args) {
-        //tipos primitivos
+        // tipos primitivos
 
-        //byte idade = 31;
-       // short ano = 2023;
-       // int cep = 12031650;
-       // long cpf = 40549134824l;
-       // float pi = 3.14F;
-       // double salario = 2500.50; 
+        // byte idade = 31;
+        // short ano = 2023;
+        // int cep = 12031650;
+        // long cpf = 40549134824l;
+        // float pi = 3.14F;
+        // double salario = 2500.50;
 
-       String meuNome = "GABRIEL";
-       double salarioMinimo = 2500;
+        String meuNome = "GABRIEL";
+        double salarioMinimo = 2500;
 
-       short numeroCurto = 1;
-       int numeroNormal = numeroCurto;
-       short numeroCurto2 = (short) numeroNormal;
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
 
-       int numero = 5;
+        int numero = 5;
 
-       numero = 10;      
-           
+        numero = 10;
+
     }
 }
