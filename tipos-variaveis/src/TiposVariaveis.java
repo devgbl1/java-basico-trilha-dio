f94bb712c1fe4f6cfd50374429de723bfa193e2e
Bowie <gabriel_scorpion_fut@hotmail.com>
2023-11-11 05:22:13 -0300

    fim_variaveis_e_tipos

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,24 @@
 public class TiposVariaveis {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        //tipos primitivos
+
+        //byte idade = 31;
+       // short ano = 2023;
+       // int cep = 12031650;
+       // long cpf = 40549134824l;
+       // float pi = 3.14F;
+       // double salario = 2500.50; 
+
+       String meuNome = "GABRIEL";
+       double salarioMinimo = 2500;
+
+       short numeroCurto = 1;
+       int numeroNormal = numeroCurto;
+       short numeroCurto2 = (short) numeroNormal;
+
+       int numero = 5;
+
+       numero = 10;      
+           
     }
 }
